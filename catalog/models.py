5c313d0f61defbcf43761ae9fe05a054cfd02b3f
Maria Lomets <marusya.lomets@gmail.com>
2024-06-24 23:36:50 +0300

    models added

---
+++ catalog/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование товара",
+        help_text="Введите наименование товара",
+    )
+    product_description = models.TextField(
+        verbose_name="Описание товара",
+        help_text="Введите описание товара",
+        blank=True,
+        null=True,
+    )
+    product_photo = models.ImageField(
+        upload_to="product/photo",
+        blank=True,
+        null=True,
+        verbose_name="Фото товара",
+        help_text="Загрузите фото товара",
+    )
+    category = models.ForeignKey(
+        on_delete=models.SET_NULL,
+        verbose_name="Категория товара",
+        help_text="Введите категорию товара",
+        null=True,
+        blank=True,
+        related_name='products',
+    )
+    price = models.IntegerField(
+        verbose_name="Цена за покупка", help_text="Введите стоимость товара"
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания", help_text="Введите дату создания товара"
+    )
+    updated_at = models.DateField(
+        verbose_name="Дата последнего изменения",
+        help_text="Введите дату последнего изменения",
+    )
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["category", "price"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    product_description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
