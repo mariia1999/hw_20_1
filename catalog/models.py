682144716b28f5e0657703fe1295c405e8a85cdd
Maria Lomets <marusya.lomets@gmail.com>
2024-06-25 22:44:49 +0300

    откат миграции 0002

--- catalog/models.py
+++ catalog/models.py
@@ -60,12 +60,7 @@ class Product(models.Model):
         verbose_name="Дата последнего изменения",
         help_text="Введите дату последнего изменения",
     )
-    manufactured_at = models.DateField(
-        verbose_name="Дата производства продукта",
-        help_text="Введите дату производства продукта",
-        null=True,
-        blank=True,
-    )
+
 
     class Meta:
         verbose_name = "Товар"
