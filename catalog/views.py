385fea88f00d0ebfdba409bc2797b513f45619cc
Maria Lomets <marusya.lomets@gmail.com>
2024-07-02 00:13:09 +0300

    добавлены шаблоны, контроллеры

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -7,3 +7,9 @@ def products_list(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, 'products_list.html', context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'products_detail.html', context)
